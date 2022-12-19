526cfe87a1cf825d22fe137b1a4ee66302f76831
Eugene Brodsky <ebr@users.noreply.github.com>
2022-12-19 00:46:09 -0500

    convert Globals to use the InvokePaths singleton
    this should ensure seamless backwards compatibility with everywhere Globals.root, etc, are used

--- ldm/invoke/globals.py
+++ ldm/invoke/globals.py
@@ -5,27 +5,24 @@ otherwise have to be passed through long and complex call chains.
 It defines a Namespace object named "Globals" that contains
 the attributes:
 
-  - root           - the root directory under which "models" and "outputs" can be found
+  - root           - the root directory under which e.g. "models" and "outputs" can be found
   - initfile       - path to the initialization file
+  - outdir         - output directory
+  - config         - models config file
   - try_patchmatch - option to globally disable loading of 'patchmatch' module
 '''
 
-import os
-import os.path as osp
 from argparse import Namespace
+from .paths import InvokePaths
 
 Globals = Namespace()
+Paths = InvokePaths()
 
-# This is usually overwritten by the command line and/or environment variables
-if os.environ.get('INVOKEAI_ROOT'):
-    Globals.root = osp.abspath(os.environ.get('INVOKEAI_ROOT'))
-elif os.environ.get('VIRTUAL_ENV'):
-    Globals.root = osp.abspath(osp.join(os.environ.get('VIRTUAL_ENV'), '..'))
-else:
-    Globals.root = osp.abspath(osp.expanduser('~/invokeai'))
+Globals.root = Paths.root
+Globals.initfile = Paths.initfile
+Globals.outdir = Paths.outdir
+Globals.config = Paths.config
 
-# Where to look for the initialization file
-Globals.initfile = 'invokeai.init'
-
-# Try loading patchmatch
 Globals.try_patchmatch = True
+
+Globals.debug = False
