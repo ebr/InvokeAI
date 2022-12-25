407a1a1279344923a5fccd3d5ac4070ccda7488f
Eugene Brodsky <ebr@users.noreply.github.com>
2022-12-25 15:40:59 -0500

    (docker) create unprivileged user; fix file ownership on bind-mounted volumes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# The entrypoint script will try to configure the InvokeAI runtime directory if necessary
-# Where automatic configuration is not desirable it can be bypassed by overriding the docker entrypoint
-# --skip-setup CLI switch will bypass the preflight checks, e.g.:
-# docker run --rm -it <this image> --skip-setup python3
+################################################################################################################
+# The entrypoint script will try to configure the InvokeAI runtime directory if necessary.
+# Where automatic configuration is not desirable it can be bypassed by overriding the docker entrypoint.
+#
+# Passing the --skip-setup CLI switch will bypass the preflight checks
+#   e.g.: docker run --rm -it <this image> --skip-setup python3
+#
+# Passing the CONTAINER_UID envvar will make any host files created by the container owned by the given user
+#   e.g.: docker run --rm -it -v /some/path:/mnt/invokeai -e CONTAINER_UID=$(id -u) <this image>
+################################################################################################################
+
+
+# Create a non-privileged container user
+# This is done here instead of Dockerfile because this way we can do it dynamically
+# and ensure it maps to a user on the local system
+USER_ID=${CONTAINER_UID:-1337}
+USER=invoke
+useradd --create-home --shell /bin/bash -u ${USER_ID} --non-unique --comment "container local user" ${USER}
+export HOME=/home/${USER}
 
 # Change the unprivileged user's UID to the given UID
 # UID 1000 is chosen as default due to popularity
@@ -18,7 +33,8 @@ setup() {
     if [[ ! -d "${INVOKEAI_ROOT}/models/ldm/stable-diffusion-v1" ]] ||
     [[ -z $(ls -A "${INVOKEAI_ROOT}/models/ldm/stable-diffusion-v1") ]] ||
     [[ ! -f "${INVOKEAI_ROOT}/invokeai.init" ]]; then
-        python3 ./scripts/configure_invokeai.py --yes
+        chown ${USER} ${INVOKEAI_ROOT}
+        gosu ${USER} python3 ./scripts/configure_invokeai.py --yes
     fi
 }
 
@@ -32,4 +48,4 @@ else
     shift
 fi
 
-exec "$@"
+exec gosu ${USER} "$@"
