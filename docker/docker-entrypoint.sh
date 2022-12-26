ce5c09286803cc1f54f654c940b3244e2d19087c
Eugene Brodsky <ebr@users.noreply.github.com>
2022-12-26 00:09:26 -0500

    (docker) move unprivileged user creation into the Dockerfile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -eu -o pipefail
 
-################################################################################################################
-# The entrypoint script will try to configure the InvokeAI runtime directory if necessary.
-# Where automatic configuration is not desirable it can be bypassed by overriding the docker entrypoint.
+#######################################################################################################################
+# This entrypoint script will by default try to configure the InvokeAI runtime directory first, if necessary.
+# It will then run the CMD as defined by the Dockerfile, unless overridden.
+# Where automatic configuration and/or dropping into the unprivileged user is not desirable at all,
+# this entrypoint can be bypassed by overriding the ENTRYPOINT directive.
 #
-# Passing the --skip-setup CLI switch will bypass the preflight checks
-#   e.g.: docker run --rm -it <this image> --skip-setup python3
+# Passing the --skip-setup CLI switch will bypass the config directory "preflight check" completely:
+#   docker run --rm -it <this image> --skip-setup python3 scripts/invokeai.py
 #
-# Passing the CONTAINER_UID envvar will make any host files created by the container owned by the given user
-#   e.g.: docker run --rm -it -v /some/path:/mnt/invokeai -e CONTAINER_UID=$(id -u) <this image>
-################################################################################################################
+# Setting the CONTAINER_UID envvar will ensure that any files created by the container in a mounted volume
+# are owned by the given UID:
+#   docker run --rm -it -v /some/path:/mnt/invokeai -e CONTAINER_UID=$(id -u) <this image>
+########################################################################################################################
 
-
-# Create a non-privileged container user
-# This is done here instead of Dockerfile because this way we can do it dynamically
-# and ensure it maps to a user on the local system
-USER_ID=${CONTAINER_UID:-1337}
+# Change the unprivileged user's UID to the given UID
+# UID 1000 is chosen as default due to popularity
 USER=invoke
-useradd --create-home --shell /bin/bash -u ${USER_ID} --non-unique --comment "container local user" ${USER}
-export HOME=/home/${USER}
+USER_ID=${CONTAINER_UID:-1000}
+usermod -u ${USER_ID} ${USER} 1>/dev/null
 
 # Change the unprivileged user's UID to the given UID
 # UID 1000 is chosen as default due to popularity
@@ -33,7 +33,8 @@ setup() {
     if [[ ! -d "${INVOKEAI_ROOT}/models/ldm/stable-diffusion-v1" ]] ||
     [[ -z $(ls -A "${INVOKEAI_ROOT}/models/ldm/stable-diffusion-v1") ]] ||
     [[ ! -f "${INVOKEAI_ROOT}/invokeai.init" ]]; then
-        chown ${USER} ${INVOKEAI_ROOT}
+        mkdir -p ${INVOKEAI_ROOT}
+        chown ${USER} ${INVOKEAI_ROOT} || true
         gosu ${USER} python3 ./scripts/configure_invokeai.py --yes
     fi
 }
